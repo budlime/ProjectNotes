0769ae1ee903f480f1f37409a90397729bc06825
budRich <robstenklippa@gmail.com>
2022-07-04 11:18:02 +0200

    initial commit

---
+++ MESSAGE.md
@@ -0,0 +1,8 @@
+Package control message 
+
+```JSON
+{ "keys": ["alt+n"], "command": "project_notes_open" }
+```
+
+bug/issues/enhancements:  
+https://github.com/budlime/ProjectNotes
---
+++ ProjectNotes.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2022, budRich. All rights reserved.
+# SPDX-License-Identifier: 0BSD
+
+import sublime
+import sublime_plugin
+
+class ProjectNotesOpenCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        print("Hello world!")
