c9d78f84b88a536467d48f442ec5071fee6aed69
budRich <robstenklippa@gmail.com>
2022-07-04 13:09:01 +0200

    pre release 0.07

--- ProjectNotes.py
+++ ProjectNotes.py
@@ -4,6 +4,70 @@
 import sublime
 import sublime_plugin
 
+from os.path import isdir, isfile, expanduser, split, join, basename, splitext
+from os import sep, getenv, makedirs
+
+from typing import Optional
+
+def tilde_prefix(target: str, use_tilde: bool):
+    home: Optional[str] = getenv("HOME")
+    target = expanduser(target)
+
+    # make sure directories have a trailing slash
+    if isdir(target) and target != sep and target[-1] != sep:
+        target += sep
+
+    if use_tilde and home and target.startswith(home):
+        target = "~" + target[len(home) :]
+
+    return target
+
+def add_directory_to_project(target: str, name: str) -> None:
+    win = sublime.active_window()
+    project_data = win.project_data() or {}
+    project_folders = project_data.get("folders") or []
+
+    directory = tilde_prefix(target, True)[:-1]
+
+    folder = dict(
+        path=directory,
+        name=name,
+    )
+
+    if all(folder["path"] != directory for folder in project_folders):
+        project_data.setdefault("folders", []).append(folder)
+        win.set_project_data(project_data)
+
+
 class ProjectNotesOpenCommand(sublime_plugin.WindowCommand):
+
     def run(self):
-        print("Hello world!")
+        project_path = self.window.project_file_name()
+
+        # current window has no associated project
+        if not project_path:
+            return
+
+        settings = sublime.load_settings("ProjectNotes.sublime-settings")
+
+        project_name, _ = splitext(basename(project_path))
+        note_name = settings.setdefault("note_file_name", "todo")
+        note_dir = settings.setdefault("notes_dir_path", "budlabs made me do it")
+        note_dir = expanduser(note_dir)
+
+        if not isdir(note_dir):
+            note_dir = join(sublime.packages_path(), "User", "notes")
+
+        note_path = join(note_dir, project_name, note_name)
+
+        note_dir, _ = split(note_path)
+
+        if not isdir(note_dir):
+            makedirs(note_dir)
+
+        if (settings.get("add_note_dir_to_project")):
+            add_directory_to_project(note_dir, settings.setdefault("note_dir_name", "notes"))
+
+        if not isfile(note_path):
+            sublime.status_message("Created new buffer '" + note_path + "'")
+        sublime.active_window().open_file(note_path)
--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# ProjectNotes
+This package provides a single command (`project_notes_open`).
+When the command is executed it will open the file:  
+`$notes_dir_path/$PROJECT_NAME/$note_file_name`  
+
+`ProjectNotes.sublime-settings`  
+```JSON
+{
+  "notes_dir_path": "...",
+  "note_file_name": "todo",
+  "add_note_dir_to_project": true,
+  "note_dir_name": "notes"
+}
+```
+
+If there is no project associated with the active
+window the command will not do anything.
+
+If **notes_dir_path** is not a path to an existing
+directory (example: `~/Documents/notes`), it will
+default to: SUBLIME_PACKAGE_PATH/User/notes
+(`~/sublime-text/Packages/User/notes`).
+
+If **add_note_dir_to_project** is set to true the directory
+containing the note will get added to the current project
+with the display name defined for **note_dir_name**.
+
+## Why?
+
+I often find myself wanting to keep notes, lists
+of links and other files related to a project, in its
+own directory, *outside* the project, so i don't accidentally
+commit those files to **git**.
+
+This simple package makes that workflow seamless.
+
+## keybinding
+
+There is no keybinding enabled by default, I personally use
+<key>Alt</key>+<key>n</key>:  
+```JSON
+{ "keys": ["alt+n"], "command": "project_notes_open" }
+```
+
+## contact
+
+bug/issues/enhancements:  
+https://github.com/budlime/ProjectNotes
+
+## license
+
+### 0BSD
